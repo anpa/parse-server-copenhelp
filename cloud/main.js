68f2a3338040d49b83bc45c405bafef3f66c0374
Andre Alves <andrenpalves@gmail.com>
2017-01-22 21:06:54 +0100

    Add cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,18 @@
+var findByFilter = require('../lib/find_by_filter'),
+    findById = require('../lib/find_by_id'),
+    feedback = require('../lib/feedback');
+
+Parse.Cloud.define('findByFilter', function(request, response) {
+  findByFilter(request.params, response);
+});
+
+Parse.Cloud.define('findById', function(request, response) {
+  findById(request.params, response);
+});
+
+Parse.Cloud.define('sendFeedback', function(request, response) {
+  feedback(request.params, response);
+});
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
